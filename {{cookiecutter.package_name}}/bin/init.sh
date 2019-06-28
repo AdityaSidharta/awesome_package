2abf14e6613d0eca59696f5d9bebcdad9a6c38c9
Aditya Sidharta <aditya.sdrt@gmail.com>
2019-06-28 15:45:38 +0800

    Debug init py

--- {{cookiecutter.package_name}}/bin/init.sh
+++ {{cookiecutter.package_name}}/bin/init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git init
-git remote add origin {{cookiecutter.project_github_origin}}
+git remote add origin {{cookiecutter.github_project_origin}}
 pyenv install 3.7.2
 pyenv local 3.7.2
 pip install pipenv
